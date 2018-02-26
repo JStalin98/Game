66ebcf5030d6b3ab776ecca5e5d5c964e69fa48a
Unknown <jsandrade1998@gmail.com>
2018-02-26 17:23:15 +0100

    Commit Inicial

---
+++ desktop/src/com/pmdm/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pmdm.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pmdm.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
