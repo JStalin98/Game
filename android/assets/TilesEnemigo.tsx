daef679ba1c4805cf4d33cb3ebae300a858963de
Unknown <jsandrade1998@gmail.com>
2018-02-28 21:12:09 +0100

    Commit 2

---
+++ android/assets/TilesEnemigo.tsx
@@ -0,0 +1,250 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TilesEnemigo" tilewidth="567" tileheight="556" tilecount="82" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (1).png"/>
+ </tile>
+ <tile id="1">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (2).png"/>
+ </tile>
+ <tile id="2">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (3).png"/>
+ </tile>
+ <tile id="3">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (4).png"/>
+ </tile>
+ <tile id="4">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (5).png"/>
+ </tile>
+ <tile id="5">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (6).png"/>
+ </tile>
+ <tile id="6">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (7).png"/>
+ </tile>
+ <tile id="7">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (8).png"/>
+ </tile>
+ <tile id="8">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (9).png"/>
+ </tile>
+ <tile id="9">
+  <image width="562" height="519" source="TilesJugadores/pngrobot/Dead (10).png"/>
+ </tile>
+ <tile id="10">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (1).png"/>
+ </tile>
+ <tile id="11">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (2).png"/>
+ </tile>
+ <tile id="12">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (3).png"/>
+ </tile>
+ <tile id="13">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (4).png"/>
+ </tile>
+ <tile id="14">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (5).png"/>
+ </tile>
+ <tile id="15">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (6).png"/>
+ </tile>
+ <tile id="16">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (7).png"/>
+ </tile>
+ <tile id="17">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (8).png"/>
+ </tile>
+ <tile id="18">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (9).png"/>
+ </tile>
+ <tile id="19">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Idle (10).png"/>
+ </tile>
+ <tile id="20">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (1).png"/>
+ </tile>
+ <tile id="21">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (2).png"/>
+ </tile>
+ <tile id="22">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (3).png"/>
+ </tile>
+ <tile id="23">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (4).png"/>
+ </tile>
+ <tile id="24">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (5).png"/>
+ </tile>
+ <tile id="25">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (6).png"/>
+ </tile>
+ <tile id="26">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (7).png"/>
+ </tile>
+ <tile id="27">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (8).png"/>
+ </tile>
+ <tile id="28">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (9).png"/>
+ </tile>
+ <tile id="29">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Jump (10).png"/>
+ </tile>
+ <tile id="30">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (1).png"/>
+ </tile>
+ <tile id="31">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (2).png"/>
+ </tile>
+ <tile id="32">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (3).png"/>
+ </tile>
+ <tile id="33">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (4).png"/>
+ </tile>
+ <tile id="34">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (5).png"/>
+ </tile>
+ <tile id="35">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (6).png"/>
+ </tile>
+ <tile id="36">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (7).png"/>
+ </tile>
+ <tile id="37">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpMelee (8).png"/>
+ </tile>
+ <tile id="38">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpShoot (1).png"/>
+ </tile>
+ <tile id="39">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpShoot (2).png"/>
+ </tile>
+ <tile id="40">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpShoot (3).png"/>
+ </tile>
+ <tile id="41">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpShoot (4).png"/>
+ </tile>
+ <tile id="42">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/JumpShoot (5).png"/>
+ </tile>
+ <tile id="43">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (1).png"/>
+ </tile>
+ <tile id="44">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (2).png"/>
+ </tile>
+ <tile id="45">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (3).png"/>
+ </tile>
+ <tile id="46">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (4).png"/>
+ </tile>
+ <tile id="47">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (5).png"/>
+ </tile>
+ <tile id="48">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (6).png"/>
+ </tile>
+ <tile id="49">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (7).png"/>
+ </tile>
+ <tile id="50">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Melee (8).png"/>
+ </tile>
+ <tile id="51">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (1).png"/>
+ </tile>
+ <tile id="52">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (2).png"/>
+ </tile>
+ <tile id="53">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (3).png"/>
+ </tile>
+ <tile id="54">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (4).png"/>
+ </tile>
+ <tile id="55">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (5).png"/>
+ </tile>
+ <tile id="56">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (6).png"/>
+ </tile>
+ <tile id="57">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (7).png"/>
+ </tile>
+ <tile id="58">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Run (8).png"/>
+ </tile>
+ <tile id="59">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (1).png"/>
+ </tile>
+ <tile id="60">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (2).png"/>
+ </tile>
+ <tile id="61">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (3).png"/>
+ </tile>
+ <tile id="62">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (4).png"/>
+ </tile>
+ <tile id="63">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (5).png"/>
+ </tile>
+ <tile id="64">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (6).png"/>
+ </tile>
+ <tile id="65">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (7).png"/>
+ </tile>
+ <tile id="66">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (8).png"/>
+ </tile>
+ <tile id="67">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/RunShoot (9).png"/>
+ </tile>
+ <tile id="68">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Shoot (1).png"/>
+ </tile>
+ <tile id="69">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Shoot (2).png"/>
+ </tile>
+ <tile id="70">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Shoot (3).png"/>
+ </tile>
+ <tile id="71">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Shoot (4).png"/>
+ </tile>
+ <tile id="72">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (1).png"/>
+ </tile>
+ <tile id="73">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (2).png"/>
+ </tile>
+ <tile id="74">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (3).png"/>
+ </tile>
+ <tile id="75">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (4).png"/>
+ </tile>
+ <tile id="76">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (5).png"/>
+ </tile>
+ <tile id="77">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (6).png"/>
+ </tile>
+ <tile id="78">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (7).png"/>
+ </tile>
+ <tile id="79">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (8).png"/>
+ </tile>
+ <tile id="80">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (9).png"/>
+ </tile>
+ <tile id="81">
+  <image width="567" height="556" source="TilesJugadores/pngrobot/Slide (10).png"/>
+ </tile>
+</tileset>
---
+++ android/assets/TilesMapa.tsx
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TilesMapa" tilewidth="357" tileheight="464" tilecount="37" columns="5">
+ <grid orientation="orthogonal" width="12" height="12"/>
+ <terraintypes>
+  <terrain name="Nuevo Terreno" tile="7"/>
+ </terraintypes>
+ <tile id="0">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Acid (1).png"/>
+ </tile>
+ <tile id="1" terrain=",,0,">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Acid (2).png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/BGTile (1).png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/BGTile (2).png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/BGTile (3).png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/BGTile (4).png"/>
+ </tile>
+ <tile id="6" terrain="0,0,0,0">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/BGTile (5).png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/BGTile (6).png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/BGTile (7).png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Fence (1).png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Fence (2).png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Fence (3).png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Spike.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (1).png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (2).png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (3).png"/>
+ </tile>
+ <tile id="16">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (4).png"/>
+ </tile>
+ <tile id="17">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (5).png"/>
+ </tile>
+ <tile id="18">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (6).png"/>
+ </tile>
+ <tile id="19">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (7).png"/>
+ </tile>
+ <tile id="20">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (8).png"/>
+ </tile>
+ <tile id="21">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (9).png"/>
+ </tile>
+ <tile id="22">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (10).png"/>
+ </tile>
+ <tile id="23">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (11).png"/>
+ </tile>
+ <tile id="24">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (12).png"/>
+ </tile>
+ <tile id="25">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (13).png"/>
+ </tile>
+ <tile id="26">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (14).png"/>
+ </tile>
+ <tile id="27">
+  <image width="256" height="256" source="TilesMapa/png/Tiles/Tile (15).png"/>
+ </tile>
+ <tile id="28">
+  <image width="177" height="238" source="TilesMapa/png/Objects/Barrel (1).png"/>
+ </tile>
+ <tile id="29">
+  <image width="177" height="238" source="TilesMapa/png/Objects/Barrel (2).png"/>
+ </tile>
+ <tile id="30">
+  <image width="256" height="256" source="TilesMapa/png/Objects/Box.png"/>
+ </tile>
+ <tile id="31">
+  <image width="280" height="464" source="TilesMapa/png/Objects/DoorLocked.png"/>
+ </tile>
+ <tile id="32">
+  <image width="280" height="464" source="TilesMapa/png/Objects/DoorOpen.png"/>
+ </tile>
+ <tile id="33">
+  <image width="281" height="464" source="TilesMapa/png/Objects/DoorUnlocked.png"/>
+ </tile>
+ <tile id="34">
+  <image width="357" height="361" source="TilesMapa/png/Objects/Saw.png"/>
+ </tile>
+ <tile id="35">
+  <image width="72" height="257" source="TilesMapa/png/Objects/Switch (1).png"/>
+ </tile>
+ <tile id="36">
+  <image width="72" height="257" source="TilesMapa/png/Objects/Switch (2).png"/>
+ </tile>
+</tileset>
